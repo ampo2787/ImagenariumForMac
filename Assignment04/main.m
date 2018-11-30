f142e2f12c90677ec76039d6896ceee37200a93c
JIHOON PARK <ampo2787@cnu.ac.kr>
2018-11-30 16:54:17 +0900

    Initial Commit

---
+++ Assignment04/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Assignment04
+//
+//  Created by JihoonPark on 30/11/2018.
+//  Copyright © 2018 JihoonPark. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
